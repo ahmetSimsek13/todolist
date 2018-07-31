86c03b9d4be78f92be06a92c1edde2507fa25790
eahmsim <ahmet.simsek@ericsson.com>
2018-07-31 14:48:51 +0300

    First commit

---
+++ config.js
@@ -0,0 +1,7 @@
+var configs = {};
+configs.applicationPort = 3000;
+configs.dbName = 'crossovertodoassignment';
+configs.dbHost = 'ahmet13:ahmet13@ds149132.mlab.com';
+configs.dbPort = 49132;
+
+module.exports = configs;
